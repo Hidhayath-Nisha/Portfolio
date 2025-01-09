6f40ba616ad432357321414b067b3c72af8c62ba
Hidhayath-Nisha <nishaidris25@gmail.com>
2025-01-08 23:48:13 -0500

    Partial Education

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const hamburger = document.getElementById('hamburger-menu');
 const menu = document.getElementById('menu-options');
+const closeButton = document.getElementById('close-btn');
+
 
 hamburger.addEventListener('click', function() {
   menu.classList.toggle('open');
+});
+
+closeButton.addEventListener('click', function() {
+  menu.classList.remove('open');
 });
